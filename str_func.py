f3203ee27d44519ffa1af1afa43ac30a37111c8a
Mikhail Baranov <problemaprod@gmail.com>
2023-04-03 19:42:47 +0100

    add title function and fix upper function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def upper_str(value):
+    """
+    делает все буквы большими
+    """
     return value.upper()
+
+
+def title_str(value):
+    """
+    делает заглавные буквы большими
+    """
+    return value.title()
